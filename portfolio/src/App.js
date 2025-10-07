603bc0174f1256df4fbbb5111884cf36b21b8ffb
Juan ACEVEDO <arthur.caron.lebrec1989@gmail.com>
2025-10-07 23:16:38 +0200

    test ajout de home.jsx

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import {Routes, Route } from "react-router-dom";
+import Home from "./home";
+import BasicExample from "./home";
+import { useRef } from "react";
+
+const divRef = useRef
+
 class App extends React.Component {
   render() {
     return(
-      <div>
-        <h1>Bienvenue sur mon application!</h1>
-      </div>
+      <>
+        <div>
+          <h1>Bienvenue sur mon application!</h1>
+        </div>
+        <div ref={divRef} className="App">
+          <BasicExample />
+
+          <Routes>
+
+            <Route path="/" element={<Home />} />
+
+          </Routes>
+
+        </div>
+        </>
     )
   }
 }
