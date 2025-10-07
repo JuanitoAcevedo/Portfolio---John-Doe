19609e73e21ad77b3c1c2606d5c6439dc9cb5c5d
Juan ACEVEDO <arthur.caron.lebrec1989@gmail.com>
2025-10-07 22:06:56 +0200

    install  react

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Bienvenue sur mon application!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
