9066fa08c62ddb229846c838a267fb73d92c4e0b
Juan ACEVEDO <arthur.caron.lebrec1989@gmail.com>
2025-12-16 20:37:32 +0100

    finalisation Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -50,11 +50,11 @@ const Footer = () => {
           <div className="col-md-4 mb-4">
             <h5 className="fw-bold">Liens utiles</h5>
             <ul className="list-unstyled">
-              <li><Link to="/" className="text-white text-decoration-none">Accueil</Link></li>
-              <li><Link to="services" className="text-white text-decoration-none">Services</Link></li>
-              <li><Link to="portfolio" className="text-white text-decoration-none">Portfolio</Link></li>
-              <li><Link to="contact" className="text-white text-decoration-none">Contact</Link></li>
-              <li><Link to="Mentions-legales" className="text-white text-decoration-none">Mentions légales</Link></li>
+              <li><Link to="/" className="text-white text-decoration-none" rel='nofollow'>Accueil</Link></li>
+              <li><Link to="services" className="text-white text-decoration-none" rel='nofollow'>Services</Link></li>
+              <li><Link to="portfolio" className="text-white text-decoration-none" rel='nofollow'>Portfolio</Link></li>
+              <li><Link to="contact" className="text-white text-decoration-none" rel='nofollow'>Contact</Link></li>
+              <li><Link to="Mentions-legales" className="text-white text-decoration-none" rel='nofollow'>Mentions légales</Link></li>
             </ul>
           </div>
 
@@ -62,9 +62,9 @@ const Footer = () => {
           <div className="col-md-4 mb-4">
             <h5 className="fw-bold">Mes dernières réalisations</h5>
             <ul className="list-unstyled">
-              <li>Portfolio React 2025</li>
-              <li>Reproduction d'un site fictif Probeats</li>
-              <li>Mon Curriculum Vitae</li>
+              <li><Link to='#' className="text-white text-decoration-none" rel='nofollow'>Portfolio React 2025</Link></li>
+              <li><Link to='#' className="text-white text-decoration-none" rel='nofollow'>Reproduction d'un site fictif Probeats</Link></li>
+              <li><Link to='#' className="text-white text-decoration-none" rel='nofollow'>Mon Curriculum Vitae</Link></li>
             </ul>
           </div>
         </div>
